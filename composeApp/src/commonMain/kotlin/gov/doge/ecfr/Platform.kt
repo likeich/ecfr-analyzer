b5021ed1e0a28960cfb5ba470f9d495f36c72d51
Kyle Eichlin <57882701+likeich@users.noreply.github.com>
2025-02-09 21:58:00 -1000

    Clean gh-pages

--- composeApp/src/commonMain/kotlin/gov/doge/ecfr/Platform.kt
+++
@@ -1,7 +0,0 @@
-package gov.doge.ecfr
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/commonMain/kotlin/gov/doge/ecfr/core/screens/DogeScreen.kt
+++
@@ -1,9 +0,0 @@
-package gov.doge.ecfr.core.screens
-
-import cafe.adriel.voyager.core.screen.Screen
-import org.jetbrains.compose.resources.DrawableResource
-
-abstract class DogeScreen : Screen {
-    abstract val icon: DrawableResource
-    abstract val title: String
-}
